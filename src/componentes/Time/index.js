21a6fd4d4ae683ca924406ce5b8635b9cd65d560
VickyPrado <vicky.prado05@gmail.com>
2025-03-19 19:19:52 -0300

    add-botão-deletar

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -6,7 +6,7 @@ const Time = ({ time, colaboradores, aoDeletar }) => {
         colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: time.corPrimaria }}>
             <h3 style={{ borderColor: time.corSecundaria }}>{time.nome}</h3>
             <div className='colaboradores'>
-                {colaboradores.map((colaborador, indice) => {                                    
+                {colaboradores.map((colaborador, indice) => {                                                     
                     return <Colaborador key={indice} colaborador={colaborador} corDeFundo={time.corSecundaria} aoDeletar={aoDeletar} />
                 })}
             </div>
